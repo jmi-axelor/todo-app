8e42c1eda42438b087bf2229834911b63359225e
Jonathan Michelon <j.michelon@axelor.com>
2016-03-30 14:49:27 +0200

    test

--- src/main/java/com/todoapp/Todo.java
+++ src/main/java/com/todoapp/Todo.java
@@ -3,14 +3,17 @@ package com.todoapp;
 import java.util.Date;
 
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
 
 import com.mongodb.BasicDBObject;
 
 /**
  * Created by shekhargulati on 09/06/14.
  */
+@Entity
 public class Todo {
-
+	@Id
     private String id;
     private String title;
     private boolean done;
@@ -21,6 +24,10 @@ public class Todo {
         this.title = dbObject.getString("title");
         this.done = dbObject.getBoolean("done");
         this.createdOn = dbObject.getDate("createdOn");
+    }
+    
+    public Todo(){
+    	
     }
 
     public String getTitle() {
